10d737ce88eef454e0be9dbd72b0fd5a5127d0fa
Noah Davis <noahadvs@gmail.com>
2017-09-30 17:33:56 -0400

    added title bar shadow images and scripts

---
+++ customizations/title_bar-gradient-shadow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+inkscape -f ../title/back-gradient-shadow.svg -e "../title/back.png"
+inkscape -f ../title/left-gradient-shadow.svg -e "../title/left.png"
+inkscape -f ../title/right-gradient-shadow.svg -e "../title/right.png"
--- customizations/title_bar-gradient.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Convert back_gradient.svg, left_gradient.svg and right_gradient.svg to
-# back.png, left.png and right.png.
-
-inkscape -f ../title/back_gradient.svg -e "../title/back.png"
-inkscape -f ../title/left_gradient.svg -e "../title/left.png"
-inkscape -f ../title/right_gradient.svg -e "../title/right.png"
