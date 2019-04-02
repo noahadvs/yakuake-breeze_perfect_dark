be3042bd28e711fd8d081745c1a074e820f38ee2
Noah Davis <noahadvs@gmail.com>
2019-04-02 15:54:48 -0400

    Remove shadows

--- customizations/title_bar-gradient-shadow.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-inkscape -f ../title/back-gradient-shadow.svg -e "../title/back.png"
-inkscape -f ../title/left-gradient-shadow.svg -e "../title/left.png"
-inkscape -f ../title/right-gradient-shadow.svg -e "../title/right.png"
--- customizations/title_bar-no_gradient-shadow.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-inkscape -f ../title/back-no_gradient-shadow.svg -e "../title/back.png"
-inkscape -f ../title/left-no_gradient-shadow.svg -e "../title/left.png"
-inkscape -f ../title/right-no_gradient-shadow.svg -e "../title/right.png"
